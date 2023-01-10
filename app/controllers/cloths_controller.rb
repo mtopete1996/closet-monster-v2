ffd9f72ea5e197921248b50ec75fa16599f4ce25
Manuel Topete <manueltopt96@gmail.com>
2023-01-10 15:39:24 -0600

    Add inline edit functionality

--- app/controllers/cloths_controller.rb
+++ app/controllers/cloths_controller.rb
@@ -5,9 +5,31 @@ class ClothsController < ApplicationController
     @cloths = cloths
   end
 
+  def edit
+    @cloth = cloth
+  end
+
+  def show
+    @cloth = cloth
+  end
+
+  def update
+    respond_to do |format|
+      format.html { render(cloth) } if cloth.update(update_params)
+    end
+  end
+
   private
 
   def cloths
-    @cloths ||= Cloth.all
+    @cloths ||= Cloth.by_name
+  end
+
+  def cloth
+    @cloth ||= Cloth.find(params[:id])
+  end
+
+  def update_params
+    params.require(:cloth).permit(:name)
   end
 end
