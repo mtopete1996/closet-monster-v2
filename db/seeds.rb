ee2c73ea0f2cb56efefe3a827a76364e9a0eaf99
Manuel Topete <manueltopt96@gmail.com>
2023-02-09 10:50:40 -0600

    Create cloths seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with
-# db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+models = [Cloth]
+
+def resource_json(model)
+  file = model.to_s.pluralize.downcase
+  path = File.join(Rails.root, 'db', 'seeds', "#{file}.json")
+  JSON.parse(File.read(path))
+end
+
+models.each do |model|
+  model_name = model.to_s.pluralize.downcase
+  puts("Creating #{model_name}...")
+  model.create(resource_json(model))
+end
+
+puts('Done!')
