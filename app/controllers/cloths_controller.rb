6cd275a1ebd579edc60acccfd2d5ca0607af6fd3
Manuel Topete <manueltopt96@gmail.com>
2023-03-22 17:33:21 -0600

    Inline visualy

--- app/controllers/cloths_controller.rb
+++ app/controllers/cloths_controller.rb
@@ -9,7 +9,19 @@ class ClothsController < ApplicationController
     @cloth = Cloth.new
   end
 
+  def create
+    @cloth = Cloth.new(cloth_params)
+
+    return render :create if @cloth.save
+
+    render :new, flash: { error: @cloth.errors.full_messages }
+  end
+
   private
+
+  def cloth_params
+    params.require(:cloth).permit(:name)
+  end
 
   def cloths
     @cloths ||= Cloth.all
